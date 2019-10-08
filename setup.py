a3516d53231a30ea61607613470435ec26fdda8e
Gerardo Figueroa <gerardo.figueroa@sentiance.com>
2019-10-08 16:25:21 +0200

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@ from setuptools import setup
 
 setup(
     name='CryptonalysisServer',
-    version='0.1',
+    version='0.1.0',
     packages=['cryptonalysis'],
     include_package_data=True,
     install_requires=[
         'flask',
     ],
+    tests_require=[
+        'nose==1.3.7'
+    ],
+    test_suite='nose.collector'
 )
